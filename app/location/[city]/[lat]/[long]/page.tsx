3a8d5dd959471fa7a5a83a9e88d15d03d68518d2
Issy786 <issy_02@hotmail.com>
2023-06-12 22:07:48 +0100

    implemented apollo client and added getClient function

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,3 +1,6 @@
+import { getClient } from "@/apollo-client";
+import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
+
 type Props = {
   params: {
     city: string;
@@ -6,7 +9,35 @@ type Props = {
   };
 };
 
-function WeatherPage({ params: { city, lat, long } }: Props) {
+async function WeatherPage({ params: { city, lat, long } }: Props) {
+  const client = getClient();
+
+  // let test = null;
+
+  // if (typeof window !== "undefined") {
+  // This code will only run on the client side
+
+  // try {
+  const { data } = await client.query({
+    query: fetchWeatherQuery,
+    variables: {
+      current_weather: "true",
+      longitude: long,
+      latitude: lat,
+      timezone: "GMT",
+    },
+  });
+
+  const results: Root = data.myQuery;
+
+  //   test = results;
+  // } catch (error) {
+  //   console.error("There was an error!", error);
+  // }
+  // console.log(results, "here");
+
+  console.log(results);
+
   return (
     <div>
       Welcome to the Weather page: {city} {lat} {long}
