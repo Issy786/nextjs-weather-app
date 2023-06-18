f5ac123bf3c1cb4051175c781f150056b30af5ed
Issy786 <issy_02@hotmail.com>
2023-06-18 19:00:23 +0100

    finished styling cards

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -54,11 +54,11 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
             </p>
           </div>
 
-          <div>
+          <div className="m-2 mb-10">
             <CalloutCard message="This is where GPT-4 summary will go" />
           </div>
 
-          <div>
+          <div className="grid grid-cols-1 xl:grid-cols-2 gap-5 m-2">
             <StatCard
               title="Maximum Temperature"
               metric={`${results.daily.temperature_2m_max[0].toFixed(1)}°`}
@@ -70,6 +70,33 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
               metric={`${results.daily.temperature_2m_min[0].toFixed(1)}°`}
               color="green"
             />
+
+            <div>
+              <StatCard
+                title="UV Index"
+                metric={`${results.daily.uv_index_max[0].toFixed(1)}°`}
+                color="rose"
+              />
+              {Number(results.daily.uv_index_max[0].toFixed(1)) > 5 && (
+                <CalloutCard
+                  message="The UV is high today, be sure to wear SPF!"
+                  warning
+                />
+              )}
+            </div>
+
+            <div className="flex space-x-3">
+              <StatCard
+                title="Wind Speed"
+                metric={`${results.current_weather.windspeed.toFixed(1)}m/s`}
+                color="cyan"
+              />
+              <StatCard
+                title="Wind Direction"
+                metric={`${results.current_weather.winddirection.toFixed(1)}°`}
+                color="violet"
+              />
+            </div>
           </div>
         </div>
       </div>
