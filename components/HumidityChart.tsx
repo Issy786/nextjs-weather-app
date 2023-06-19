7ea1812ebf45acad21279bfffdbb3c12a13825b6
Issy786 <issy_02@hotmail.com>
2023-06-19 15:54:04 +0100

    debugged time for graphs

--- components/HumidityChart.tsx
+++ components/HumidityChart.tsx
@@ -11,7 +11,7 @@ function HumidityChart({ results }: Props) {
     .map((time) =>
       new Date(time).toLocaleString("en-US", {
         hour: "numeric",
-        hour12: true,
+        hour12: false,
       })
     )
     .slice(0, 24);
